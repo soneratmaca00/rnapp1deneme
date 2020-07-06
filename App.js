e33f182dcb5ac2b6aa16940fb579140446477f3b
snrtmc <soneratmaca00@gmail.com>
2020-07-06 14:05:15 +0300

    First commit

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import Logo from './proje/decipherlogo';
-import Deneme from './proje/deneme';
-import ViewPager from './proje/ViewPager';
-import Dashboard from './proje/Dashboard';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Signup from './proje/Signup';
-import Login from './proje/Login';
-import Deposit from './proje/Deposit';
+import { View, Text, Button,Image } from 'react-native';
+import Oyun from './Game/Oyun'
 
 
-const Stack = createStackNavigator();
+
+
 
 
 function App() {
@@ -19,21 +12,10 @@ function App() {
   
   
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="ViewPager"  >
-        <Stack.Screen name="ViewPager" component={ViewPager}
-        options={{ headerShown: false }}          
-        />
-        <Stack.Screen name="Login" component={Login}
-        options={{ headerShown: false }} />
-        <Stack.Screen name="Dashboard" component={Dashboard}
-        options={{ headerShown: false }} />
-        <Stack.Screen name="Signup" component={Signup} 
-        options={{ headerShown: false }}  />
-        <Stack.Screen name="Deposit" component={Deposit} 
-        options={{ headerShown: false }}  />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+    <Oyun/>
+    
+    </View>
   );
 }
 
--- Game/Oyun.js
+++ Game/Oyun.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     View, Image,
     ImageBackground, Button, Text,
-    StyleSheet, FlatList, styles, TouchableHighlight,indexOf
+    StyleSheet, FlatList, TouchableHighlight,indexOf
 } from 'react-native';
+
 
 let firstTap = null
 let secondTap = null
@@ -12,26 +13,35 @@ let usedNumber=[]
 
 
 let apeople = [
-    { name: 'soner', id: 1, kartId: 1, visible: false,   status:true,   source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/1200px-Facebook_icon.svg.png' },
-    { name: 'adnan', id: 2, kartId: 2, visible: false,   status:true,   source: 'https://previews.123rf.com/images/marco1987/marco19871803/marco1987180300424/96588089-planet-earth-icon-.jpg' },
-    { name: 'arda',  id: 3, kartId: 3, visible: false,   status:true,   source: 'https://image.shutterstock.com/image-vector/coronavirus-2019ncov-corona-virus-icon-600w-1644823183.jpg' },
-    { name: 'gökha', id: 4, kartId: 4, visible: false,   status:true,   source: 'https://surucukursumuz.com/wp-content/uploads/2018/11/icon.png' },
-    { name: 'soner', id: 5, kartId: 1, visible: false,   status:true,   source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/1200px-Facebook_icon.svg.png' },
-    { name: 'adnan', id: 6, kartId: 2, visible: false,   status:true,   source: 'https://previews.123rf.com/images/marco1987/marco19871803/marco1987180300424/96588089-planet-earth-icon-.jpg' },
-    { name: 'ardaa', id: 7, kartId: 3, visible: false,   status:true,   source: 'https://image.shutterstock.com/image-vector/coronavirus-2019ncov-corona-virus-icon-600w-1644823183.jpg' },
-    { name: 'gökh', id: 8, kartId: 4, visible: false,   status:true,   source: 'https://surucukursumuz.com/wp-content/uploads/2018/11/icon.png' },
+    { name: 'alchemy', id: 1,  kartId: 1, visible: false,   status:true,   source: require('./alchemy.png') },
+    { name: 'baby',    id: 2,  kartId: 2, visible: false,   status:true,   source: require('./baby.png') },
+    { name: 'batman',  id: 3,  kartId: 3, visible: false,   status:true,   source: require('./batman.png') },
+    { name: 'eagle',   id: 4,  kartId: 4, visible: false,   status:true,   source: require('./eagle.png') },
+    { name: 'alchemy', id: 5,  kartId: 1, visible: false,   status:true,   source: require('./alchemy.png') },
+    { name: 'baby',    id: 6,  kartId: 2, visible: false,   status:true,   source: require('./baby.png') },
+    { name: 'batman',  id: 7,  kartId: 3, visible: false,   status:true,   source: require('./batman.png') },
+    { name: 'eagle',   id: 8,  kartId: 4, visible: false,   status:true,   source: require('./eagle.png') },
+    { name: 'phone',   id: 9,  kartId: 5, visible: false,   status:true,   source: require('./phone.png') },
+    { name: 'pistol',  id: 10, kartId: 6, visible: false,   status:true,   source: require('./pistol.png') },
+    { name: 'mustache',id: 11, kartId: 7, visible: false,   status:true,   source: require('./mustache.png') },
+    { name: 'einstein',id: 12, kartId: 8, visible: false,   status:true,   source: require('./einstein.png') },
+    { name: 'phone',   id: 13, kartId: 5, visible: false,   status:true,   source: require('./phone.png') },
+    { name: 'pistol',  id: 14, kartId: 6, visible: false,   status:true,   source: require('./pistol.png') },
+    { name: 'mustache',id: 15, kartId: 7, visible: false,   status:true,   source: require('./mustache.png') },
+    { name: 'einstein',id: 16, kartId: 8, visible: false,   status:true,   source: require('./einstein.png') },
+    
 ]
 
 let bpeople =[]
 
 for( let i=apeople.length;  apeople.length - usedNumber.length  > 0; i--){
     
-    let rndmSayi = Math.floor((Math.random() * 8) + 1)
+    let rndmSayi = Math.floor((Math.random() * 16) + 1)
     
         if(usedNumber.indexOf(rndmSayi) > -1){
 
         }else{
-            console.log('rndmsayi', rndmSayi, 'apeople', apeople[rndmSayi - 1])
+           // console.log('rndmsayi', rndmSayi, 'apeople', apeople[rndmSayi - 1])
         bpeople.push(apeople[rndmSayi-1])
         
         usedNumber=[... usedNumber, rndmSayi]
@@ -40,64 +50,85 @@ for( let i=apeople.length;  apeople.length - usedNumber.length  > 0; i--){
     
    
 }
-console.log('bpeople',bpeople)
+//console.log('bpeople',bpeople)
+let timer1=0
 
 export default function Oyun() {
+
+
+    const [timer, setTimer] = useState(0)
+    useEffect(() => {
+        
+        timer2 = setInterval(() => {
+        timer1=timer1 + 1
+        setTimer(timer1)
+        
+        
+        }, 1000);
+        
+    },[])
 
    
     const [people, setPeople] = useState([...bpeople
     ])
 
-    
+   
     
     const [gorunus, setGorunus] = useState('none')
 
     
 
     const clickHandler = (item) => {
-
+        
         const newPeople = [...people]
         
         
         if(!item.status){
-            console.log('önceden bilinmiş')
+           // console.log('önceden bilinmiş')
         }else{
             if (!firstTap) {
                 //ilk değişken atanıyor
                 firstTap = item
                 
                   
-                  console.log('index',newPeople.indexOf(item));
+                  // console.log('index',newPeople.indexOf(item));
                 newPeople[newPeople.indexOf(item)].visible = !newPeople[newPeople.indexOf(item)].visible
                 
                 setPeople(newPeople)
-                console.log(firstTap)
+                // console.log(firstTap)
                 
             } else if (!secondTap) {
                             
                 if(firstTap === item){
                     //aynı kareye tıklanıp tıklanmadığı kontrol ediliyor
                     //aynısına tıklanmışsa bir şey yapılmıyor.
-                    console.log('aynısına tıklandı')
+                   // console.log('aynısına tıklandı')
                 }else{
                     //farklı bir kareye tıklanmışsa ikinci değişkene atanıyor.
                     secondTap = item
                     newPeople[newPeople.indexOf(item)].visible = !newPeople[newPeople.indexOf(item)].visible
                     setPeople(newPeople)
-                    console.log(secondTap)
+                   // console.log(secondTap)
                    
                     
                     if (firstTap.kartId == secondTap.kartId) {
                         //ilki ile ikinci karenin aynı olduğu durum
-                        console.log("doğru bildi")
+                     //   console.log("doğru bildi")
                         newPeople[newPeople.indexOf(firstTap)].status = false
                         newPeople[newPeople.indexOf(secondTap)].status = false
                         setPeople(newPeople)
                         setGorunus("tşk")
                         firstTap = null
                         secondTap = null
+                        const dizi = people.map(item => item.status)
+
+                        if(dizi.indexOf(true) === -1){
+                            clearInterval(timer2)
+                        }
+                        console.log(dizi)
+                       
                     } else {
-                        console.log('yanlış bildi')
+                   //     console.log('yanlış bildi')
                         
                             
                         /* newPeople[firstTap.id - 1].visible = false
@@ -111,20 +142,20 @@ export default function Oyun() {
                         console.log('firsttap', firstTap, 'secontap', secondTap) */
                         
                         setTimeout(() => {
-                            console.log(1, newPeople)
+                        //    console.log(1, newPeople)
                             
                             const newPeople2 = [...people]
-                            console.log(2, newPeople2)
+                        //    console.log(2, newPeople2)
                             newPeople2[newPeople.indexOf(firstTap)].visible = false
                             newPeople2[newPeople.indexOf(secondTap)].visible = false
-                            console.log(2, newPeople2)
-                                console.log("np 1", newPeople2)
+                        //    console.log(2, newPeople2)
+                          //      console.log("np 1", newPeople2)
                                 setPeople(newPeople2)
-                                console.log("np 2", newPeople2)
+                          //      console.log("np 2", newPeople2)
                                 firstTap = null
                                 secondTap = null
-                                console.log(firstTap, 'sdasd', secondTap)
-                        }, 1000);
+                           //     console.log(firstTap, 'sdasd', secondTap)
+                        }, 500);
                             
                     }
                 }
@@ -135,9 +166,11 @@ export default function Oyun() {
     
 
     return (
-        <View>
+        <View >
+        <View style={styles.timer}><Text>{timer1}</Text></View>
             <FlatList
-                numColumns={2}
+            style={styles.container}
+                numColumns={4}
                 keyExtractor={(item) => item.id}
                 data={people}
                 renderItem={({ item }) => (
@@ -147,10 +180,10 @@ export default function Oyun() {
                         clickHandler(item)
                         
                     }}>
-                        <View style={{ backgroundColor: 'orange', width: 70, height: 70, borderWidth: 1 }}>
+                        <View style={{ backgroundColor: 'white', width: 70, height: 70, borderWidth: 1 }}>
                             {
                                 item.visible ?
-                                    <Image style={{ width: 70, height: 70 }} source={{ uri: item.source }} />
+                                    <Image style={{ width: 70, height: 70 }} source={item.source} />
                                     : null
                             }
                         </View>
@@ -161,6 +194,20 @@ export default function Oyun() {
         </View>
     )
 }
+const styles =  StyleSheet.create({
+    container:{ 
+    
+       
+        
+        marginTop:150,
+        marginLeft:40,
+    },
+    timer:{
+        alignItems:'center',
+        paddingTop:20,
+    }
+
+})
 
 
 
